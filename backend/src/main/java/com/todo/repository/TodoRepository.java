3afcbb6ef21bc1400ca5176baf7bbdb46e53ad18
utro007 <aljaz.utrosa19@gmail.com>
2025-12-06 18:10:02 +0100

    Pridobivanje nalog po letu in mesecu
    Pridobivanje podatkov iz podatkovne baze

--- backend/src/main/java/com/todo/repository/TodoRepository.java
+++ backend/src/main/java/com/todo/repository/TodoRepository.java
@@ -3,8 +3,10 @@ package com.todo.repository;
 import com.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -42,4 +44,14 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
             nativeQuery = true
     )
     List<Todo> searchByTitle(String keyword);
+
+    /**
+     * Vrne vse naloge z rokom v določenem mesecu in letu.
+     */
+    @Query(
+            value = "SELECT * FROM todos WHERE deadline IS NOT NULL " +
+                    "AND YEAR(deadline) = :year AND MONTH(deadline) = :month",
+            nativeQuery = true
+    )
+    List<Todo> findByDeadlineYearAndMonth(@Param("year") int year, @Param("month") int month);
 }
